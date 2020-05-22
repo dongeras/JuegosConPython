9883e49f765ef31f9cf47a80a1e4f5f5d6fd63aa
Gerardo Rivas <dongeras@protonmail.com>
2020-05-22 15:16:52 -0500

    Ya se impementaron todas las func. basicas y el algoritmo solucion

--- hanoi.py
+++ hanoi.py
@@ -5,65 +5,147 @@ Created on Thu May 14 14:08:21 2020
 
 @author: gerardo
 """
-"""
-def postes(n):
-    #Muestra en pantalla tres postes con n discos cada uno
-    #
-    torres = [[],[],[]]
-        
-        
-    for j in range(n):
-        for i in range(3):
-            torres[i].append(" "*((n+1)-j) + "_"*(j+1) + "|" + "_"*(j+1) + " "*((n+1)-j)) 
-    
-    for i in range(n):
-        print(torres[0][i] + torres[1][i] + torres[2][i])
 
-Siento que ya no servirá. La dejamos por si las dudas
-"""
+def algoritmoHanoi(numDiscos, torreInicial, torreFinal, torreAux):
+
+        
+    if numDiscos == 1:
+        realizaMovida(configuracionActual, torreInicial, torreFinal)
+        print("Mueve el último disco de la torre %s a la torre %s" %(torreInicial+1, torreFinal+1))
+        dibujaPostes(colocaDiscos(configuracionActual, numeroDeDiscos), numeroDeDiscos)
+        
+    else:
+        
+        algoritmoHanoi(numDiscos-1, torreInicial, torreAux, torreFinal)    
+        
+        realizaMovida(configuracionActual, torreInicial, torreFinal)
+        print("Mueve el último disco de la torre %s a la torre %s" %(torreInicial+1, torreFinal+1))
+        dibujaPostes(colocaDiscos(configuracionActual, numeroDeDiscos), numeroDeDiscos)
+        
+        algoritmoHanoi(numDiscos-1, torreAux, torreFinal, torreInicial)
+        
+
 def dibujaPostes(discosEnTorres, n):
     #toma la información de en qué postes están los discos
     #y muestra en pantalla el dibujo correspondiente.
+    
+    print(' '*(n+2) + '1' + ' '*2*(n+2) + '2' + ' '*2*(n+2) + '3' )
+    print('')
     for i in range(n):
         print(discosEnTorres[0][i] + discosEnTorres[1][i] + discosEnTorres[2][i])
+    print('_' *3*(2*n+5))
+    print("|" *3*(2*n+5)  )
         
     
-def discos(n):
-    # Regresa una lista con los n discos del juego
-    listaDiscos = []
+def configuracionInicial(n):
+    # Regresa una lista con tres listas
+    # Cada una las sub-listas representa el conjunto de discos en una torre
+    listaDiscos = [[],[],[]]
     for j in range(n):
-        listaDiscos.append(" "*((n+1)-j) + "_"*(j+1) + "|" + "_"*(j+1) + " "*((n+1)-j))
+        listaDiscos[0].append(j+1)
     return listaDiscos
 
-def colocaDiscos(discosEnTorre, n):
-    #Coloca, de abajo para arriba, los discos en la lista "discosEnTorre"
-    #en una lista "torre"; llena los espacios restantes con diburjos vacíos
-    #dicosEnTorre ordenados de mayor a menor
-    torre = [" " * (n+2) + "|" + " " * (n+2) ] * n
-    for i in range(len(discosEnTorre)):
-        torre[(n-1-i)] = discosEnTorre[i]
+def colocaDiscos(configuracion, n):
+    #Prepara una lista de listas con las cadenas de texto
+    # para mostrar el Arte ASCI correspondiente a la 
+    #Configuración de discos "configuracion"
+    torre = [[], [], []]
+    for j in range(3):
+        for i in range(n-len(configuracion[j])):
+            torre[j].append(" " * (n+2) + "|" + " " * (n+2))
+        for i in range(len(configuracion[j])):
+            torre[j].append(" " * (n +2 - configuracion[j][i]) + 
+                            "_" * configuracion[j][i] +
+                            "|" + "_" * configuracion[j][i] +
+                            " " *(n + 2 - configuracion[j][i]))
     return torre
         
-def ordenDiscos(discosEnTorre):
-    #MODIFICAR!
-    #Regresa True si los discos están bien acomodados
+def jugadaValida(configuracionDiscos, torreInicial, torreFinal):
+    #Regresa True si si la jugada es válida
     #y regresa False en caso contrario
-    for i in range(1,len(discosEnTorre)):
-        #recorrer todos los discos en la lista
-        for j in range(0,i):
-            #recorre los que están por debajo del iésimo
-            if len(discosEnTorre[i]) <= len(discosEnTorre[j]):
-                return False
+    copiaConfiguracion = [[],[],[]] 
+    
+    #El sig. ciclo copia la configuración
+    
+    if len(configuracionDiscos[torreInicial]) == 0:
+        return False
+    
+    for i in range(3):
+        for disco in configuracionDiscos[i]:
+            copiaConfiguracion[i].append(disco)
+    
+    #Se hace la jugada en la copia de la conf.
+    realizaMovida(copiaConfiguracion, torreInicial, torreFinal)
+    #Se verigica que la jugada sea válida
+    if len(copiaConfiguracion[torreFinal]) > 1:
+        if copiaConfiguracion[torreFinal][0] >= copiaConfiguracion[torreFinal][1]:
+            return False
     return True
+
+def esGanadora(configuracionDiscos, n):
+    #Regresa True si la configuración de discos es la ganadora
+    #i.e todos los discos están en la última torre.
+    #Regresa False si no es la conf. ganadora
+    todosDiscos = [i for i in range(1, n+1)]
+    return set(configuracionDiscos[2]) == set(todosDiscos)
+    
+    
+    
                
-discosEnLaTorre = discos(5)
-discosEnLaTorre.reverse()
-orden = ordenDiscos(discosEnLaTorre)
+
+def movimientoUsuario(configuracionDiscos):
+    # Le pide al usuario un movimiento y verifica
+    # 1) que elija torres existente (1, 2, 3)
+    # 2) que el disco que se mueve es más chiquito que el último de la nueva torre
+    
+    respuesta = ''
+    DIGITOS = '1 2 3'.split()
+    while (len(respuesta) != 2 or respuesta[0] == respuesta[1] or  
+           (respuesta[0] not in DIGITOS and respuesta[1] not in DIGITOS) or 
+           not jugadaValida(configuracionDiscos, int(respuesta[0])-1, int(respuesta[1])-1)):
+        print('¿Cuál es tu movimiento (Indica Torre inicial y final)?')
+        respuesta = input()
+        
+    torreInicial = int(respuesta[0]) - 1
+    torreFinal = int(respuesta[1]) - 1
+        
+    return [torreInicial, torreFinal]    
+        
 
 
-"""
-Ya podemos dibujar una configuración arbitraria de torres y discos.
+def realizaMovida(discosEnTorre, torreInicial, torreFinal):
+    #Realiza la "jugada" en la configuración "discosEnTorre"
+    discosEnTorre[torreFinal].insert(0, discosEnTorre[torreInicial][0])
+    del discosEnTorre[torreInicial][0]
 
-Falta verificar que los discos en una torre están bien ordenados
-"""
-
+while True:
+    
+    print('¿Cuántos discos hay en las torres?')
+    numeroDeDiscos = int(input()) 
+    configuracionActual = configuracionInicial(numeroDeDiscos)
+    dibujaPostes(colocaDiscos(configuracionActual, numeroDeDiscos), numeroDeDiscos)
+    respuesta = ''
+    while  not respuesta.startswith('j') and not respuesta.startswith('s'):
+        print('¿Quieres jugar (j) o ver la solución (s)?')
+        respuesta = input().lower()
+    
+    if respuesta.startswith('j'):
+        numPasos = 0
+        while not esGanadora(configuracionActual, numeroDeDiscos):
+#            dibujaPostes(colocaDiscos(configuracionActual, numeroDeDiscos), numeroDeDiscos)
+            numPasos += 1
+            torreInicial, torreFinal = movimientoUsuario(configuracionActual)
+            realizaMovida(configuracionActual, torreInicial, torreFinal)
+            dibujaPostes(colocaDiscos(configuracionActual, numeroDeDiscos), numeroDeDiscos)
+            dibujaPostes(colocaDiscos(configuracionActual, numeroDeDiscos), numeroDeDiscos)
+            
+        print('El juego ha finalizado después de %s pasos.' % (numPasos))
+        if numPasos == 2**numeroDeDiscos - 1:
+            print('¡Felicidades, lo resolviste en el menor número de pasos!')
+    elif respuesta.startswith('s'):
+        dibujaPostes(colocaDiscos(configuracionActual, numeroDeDiscos), numeroDeDiscos)
+        algoritmoHanoi(numeroDeDiscos, 0, 2, 1)  
+    
+    print('¿Quieres volver a jugar? (S o N)')
+    if input().lower().startswith('n'):
+        break
